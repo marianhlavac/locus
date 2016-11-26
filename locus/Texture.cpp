96fc6be37ce4c257d7110e26451d21eca73ae83b
Marián Hlaváč <hlavac95@gmail.com>
2016-11-26 14:11:14 +0100

    Create basic Texture class

--- locus/Texture.cpp
+++ locus/Texture.cpp
@@ -7,3 +7,33 @@
 //
 
 #include "Texture.hpp"
+
+#include <SOIL/SOIL.h>
+
+Texture::Texture(unsigned char* image, int width, int height) : size(vec2(width, height)) {
+    glGenTextures(1, &texture);
+    bind();
+    generateTexture(image);
+    SOIL_free_image_data(image);
+    unbind();
+}
+
+Texture* Texture::loadFromFile(const string& filename) {
+    int width, height;
+    
+    unsigned char* image = SOIL_load_image(filename.c_str(), &width, &height, 0, SOIL_LOAD_RGB);
+    return new Texture(image, width, height);
+}
+
+void Texture::bind() {
+    glBindTexture(GL_TEXTURE_2D, texture);
+}
+
+void Texture::unbind() {
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture::generateTexture(unsigned char* image) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, size.x, size.y, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+}
--- locus/Texture.hpp
+++ locus/Texture.hpp
@@ -9,8 +9,26 @@
 #ifndef Texture_hpp
 #define Texture_hpp
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+
+#include <string>
+
+using namespace std;
+using namespace glm;
+
 class Texture {
-    
+public:
+    Texture(unsigned char* image, int width, int height);
+    static Texture* loadFromFile(const string& filename);
+    void bind();
+    static void unbind();
+private:
+    vec2 size;
+    GLuint texture;
+    void generateTexture(unsigned char* image);
 };
 
 #endif /* Texture_hpp */
